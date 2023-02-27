f7271e797e132b541e770bfad9978fd5d1009d5f
Milica Vacic <mvacic9@gmail.com>
2023-02-27 12:51:33 +0100

    ObjectInfo for record types

--- force-app/main/default/lwc/selectPersonRecordType/selectPersonRecordType.js
+++ force-app/main/default/lwc/selectPersonRecordType/selectPersonRecordType.js
@@ -1,13 +1,31 @@
 import { LightningElement, wire } from 'lwc';
-import getRecordTypes from '@salesforce/apex/PersonController.getPersonRecordTypeList';
+//import getRecordTypes from '@salesforce/apex/PersonController.getPersonRecordTypeList';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import PERSON_OBJECT from '@salesforce/schema/Person__c';
 
 export default class SelectPersonRecordType extends LightningElement {
     value;
     options;
     error;
 
+    @wire(getObjectInfo, { objectApiName: PERSON_OBJECT })
+    objectInfo({error,data}){
+        if (data) {
+            this.options={}
+            for (let rtId of data.recordTypeInfos){
+                this.options.push({label:data.recordTypeInfos[rtId].Name, value:rtId});
+            }
+            this.error = undefined;
+            this.value=this.options[0];
+        } else if (error) {
+            this.error = error;
+            this.options = undefined;
+        }
+    }
+
+
     //TODO get Record Types from https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.data_considerations instead of APEX
-    @wire(getRecordTypes)
+/*     @wire(getRecordTypes)
     wiredRecordTypes({ error, data }) {
         if (data) {
             this.options = data.map(x=>{
@@ -19,7 +37,7 @@ export default class SelectPersonRecordType extends LightningElement {
             this.error = error;
             this.options = undefined;
         }
-    }
+    } */
 
     handleChange(event) {
         this.value = event.detail.value;
