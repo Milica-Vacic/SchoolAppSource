122c2ccc9dc444f83d0bbd224c61d583420a949c
Milica Vacic <mvacic9@gmail.com>
2023-02-27 13:27:24 +0100

    fix record type retrieval, delete old method

--- force-app/main/default/lwc/selectPersonRecordType/selectPersonRecordType.js
+++ force-app/main/default/lwc/selectPersonRecordType/selectPersonRecordType.js
@@ -1,5 +1,4 @@
 import { LightningElement, wire } from 'lwc';
-//import getRecordTypes from '@salesforce/apex/PersonController.getPersonRecordTypeList';
 import { getObjectInfo } from 'lightning/uiObjectInfoApi';
 import PERSON_OBJECT from '@salesforce/schema/Person__c';
 
@@ -11,9 +10,10 @@ export default class SelectPersonRecordType extends LightningElement {
     @wire(getObjectInfo, { objectApiName: PERSON_OBJECT })
     objectInfo({error,data}){
         if (data) {
-            this.options={}
-            for (let rtId of data.recordTypeInfos){
-                this.options.push({label:data.recordTypeInfos[rtId].Name, value:rtId});
+            this.options=[];
+            for (let rtId of Object.keys(data.recordTypeInfos)){
+                if(data.recordTypeInfos[rtId].name!='Master')
+                    this.options.push({label:data.recordTypeInfos[rtId].name, value:rtId});
             }
             this.error = undefined;
             this.value=this.options[0];
@@ -22,22 +22,6 @@ export default class SelectPersonRecordType extends LightningElement {
             this.options = undefined;
         }
     }
-
-
-    //TODO get Record Types from https://developer.salesforce.com/docs/component-library/documentation/en/lwc/lwc.data_considerations instead of APEX
-/*     @wire(getRecordTypes)
-    wiredRecordTypes({ error, data }) {
-        if (data) {
-            this.options = data.map(x=>{
-                return {label:x.Name, value:x.Id};
-            });
-            this.error = undefined;
-            this.value=this.options[0];
-        } else if (error) {
-            this.error = error;
-            this.options = undefined;
-        }
-    } */
 
     handleChange(event) {
         this.value = event.detail.value;
