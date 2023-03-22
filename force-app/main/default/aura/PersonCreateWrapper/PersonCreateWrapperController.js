e13ac6e2e27c26587563285f949203c944719166
Milica Vacic <mvacic9@gmail.com>
2023-03-22 02:39:33 +0100

    personCreate refresh from aura

--- force-app/main/default/aura/PersonCreateWrapper/PersonCreateWrapperController.js
+++ force-app/main/default/aura/PersonCreateWrapper/PersonCreateWrapperController.js
@@ -1,6 +1,12 @@
 ({
-    onPageReferenceChange: function(component, event, helper) {
+    init: function(component, event, helper) {
         var pageRef=component.get("v.pageReference");
         component.set("v.recordTypeId",pageRef.state.recordTypeId);
+    },
+
+    pageRefChange: function(component, event, helper) {
+        var pageRef=component.get("v.pageReference");
+        component.set("v.recordTypeId",pageRef.state.recordTypeId);
+        $A.get('e.force:refreshView').fire();
     }
 })
