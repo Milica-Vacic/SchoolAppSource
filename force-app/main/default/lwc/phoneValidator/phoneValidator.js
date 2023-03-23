b88668ae8662c6aaff26ca7018cb198829f45a74
Milica Vacic <mvacic9@gmail.com>
2023-03-23 07:06:19 +0100

    Selective rendering based on field access

--- force-app/main/default/lwc/phoneValidator/phoneValidator.js
+++ force-app/main/default/lwc/phoneValidator/phoneValidator.js
@@ -1,9 +1,23 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import getFieldAccessability from '@salesforce/apex/LwcUtility.getFieldAccessability';
 
 export default class PhoneValidator extends LightningElement {
     @api objectApiName;
     @api recordId;
     @api fieldName;
+
+    @wire(getFieldAccessability,{objectApiName:'$objectApiName', fieldName:'$fieldName'})
+    __access;
+
+    get hasAccess(){
+        if (this.__access=='unreadable') return false;
+        else return true;
+    }
+
+    get readOnly(){
+        if (this.__access=='readable') return true;
+        else return false;
+    }
 
     get FieldName(){
         return this.fieldName.charAt(0).toUpperCase() + this.fieldName.slice(1);
