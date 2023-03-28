8f961c92264236f1a532b4a4b90f94efe3d1a3d1
Milica Vacic <mvacic9@gmail.com>
2023-03-28 02:40:05 +0200

    PhoneValidatorUtilityBar record page implement

--- force-app/main/default/lwc/phoneValidatorUtilityBar/phoneValidatorUtilityBar.js
+++ force-app/main/default/lwc/phoneValidatorUtilityBar/phoneValidatorUtilityBar.js
@@ -1,10 +1,7 @@
 import { LightningElement, api, wire } from 'lwc';
-import getFieldAccessability from '@salesforce/apex/LwcUtility.getFieldAccessability';
 import validatePhone from '@salesforce/apex/LwcUtility.validatePhone';
-import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
-import COUNTRY_FIELD from '@salesforce/schema/Person__c.Country__c';
-
-const fields = [COUNTRY_FIELD];
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import { getRecord} from 'lightning/uiRecordApi';
 
 //has record id
     //dovlaci fields into a select
@@ -14,48 +11,60 @@ const fields = [COUNTRY_FIELD];
     //samo text input boji salje apiju na potvrdu
 
 export default class PhoneValidatorUtilityBar extends LightningElement {
-    @api objectApiName;
-    @api recordId;
-    fieldName;
+    @api sObjectId;
+    sObjectName;
+    selectedField;
     hasAccess;
     readOnly;
     validationResult;
     validationDetails;
     columns;
+    phoneFields=[];
 
-    @wire(getFieldAccessability,{objectApiName:'$objectApiName', fieldName:'$fieldName'})
-    access({ error, data }) {
-        if (data) {
-            this.hasAccess=data=='unreadable'?false:true;
-            this.readOnly=data=='readable'?true:false;
-          } else if (error) {
-            this.hasAccess=false;
-            this.readOnly=false;
-          }
-    }
+    @wire(getRecord, { recordId: '$sObjectId', fields:['Name'] })
+    record({error, data}) {
+            if (error) {
+                this.hasAccess=false;
+                this.readOnly=false;
+                this.phoneFields=undefined;
+            } 
+            else if (data) {
+                this.sObjectName=data.apiName;
+            }
+        }
 
-    @wire(getRecord, { recordId: '$recordId', fields })
-    person;
+    @wire(getObjectInfo, { objectApiName: '$sObjectName'})
+        objectInfo({error, data}) {
+            if (error) {
+                this.hasAccess=false;
+                this.readOnly=false;
+                this.phoneFields=undefined;
+            } 
+            else if (data) {
+                this.phoneFields=[];
+                for(let fname of Object.keys(data.fields)){
+                    if (data.fields[fname].dataType=='Phone'){
+                        this.phoneFields.push({label:data.fields[fname].label, value:fname});
+                    }
+                }
+                this.selectedField=this.phoneFields[0].value;
+                this.hasAccess=data.fields[this.selectedField] ? true : false;
+                this.readOnly=!data.fields[this.selectedField].updateable;
 
-    get country() {
-        return getFieldValue(this.person.data, COUNTRY_FIELD);
-    }
+            }
+        }
 
 
-    get FieldName(){
-        return this.fieldName.charAt(0).toUpperCase() + this.fieldName.slice(1);
+    handleFieldChange(event){
+        this.selectedField = event.detail.value;
     }
 
     get title(){
-        return `Validate ${this.FieldName}`
-    }
-
-    handleFieldChange(){
-        
+        return `Validate ${this.selectedField}`
     }
 
     handleValidate(){
-        validatePhone({ phone:this.template.querySelector('[data-id="phoneField"]').value, country:this.country})
+        validatePhone({ phone:this.template.querySelector('[data-id="phoneField"]').value})
             .then((result)=>{
                 let res=JSON.parse(result);
                 res.Items[0].Error ? this.validationResult =`Error! ${res.Items[0].Description}: ${res.Items[0].Cause}` : 
@@ -69,7 +78,7 @@ export default class PhoneValidatorUtilityBar extends LightningElement {
     }
 
     handleViewDetails(){
-        validatePhone({ phone:this.template.querySelector('[data-id="phoneField"]').value, country:this.country})
+        validatePhone({ phone:this.template.querySelector('[data-id="phoneField"]').value})
             .then((result)=>{
                 let res=JSON.parse(result);
                 this.validationDetails=res.Items;
